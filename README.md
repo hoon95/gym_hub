8749026b2e46d82276cab818c0ad7775132f0652
hoon95 <maruon95@gmail.com>
2024-11-05 18:15:03 +0900

    Typescript module error 해결

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
-# 가제 : GymHub
+# 💻 프로젝트 소개
 
-이미지 첨부
+<img src="/public/Intro.png">
 
-```초보자들이 헬스장을 쉽게 이용할 수 있도록 다양한 정보와 프로그램을 제공하는 플랫폼```
-
-<br>
-
-## 프로젝트 소개
+`초보자들이 헬스장을 쉽게 이용할 수 있도록 다양한 정보와 프로그램을 제공하는 플랫폼`
 
 <br>
 
-## 개발 환경 및 배포
+<hr />
+
+## 📦 개발 환경 및 배포
+
 <span>
   <img src="https://img.shields.io/badge/React-1572B6?style=flat&logo=react&logoColor=white">
   <img src="https://img.shields.io/badge/Styled_Components-DB7093?style=flat&logo=styled-components&logoColor=white">
@@ -19,15 +18,16 @@
 </span>
 <br>
 
-## 프로젝트 구조
-
-
-<br>
-
-### 개발 기간
-
-2024.00.00 ~ 2024.00.00
+## 📂 프로젝트 구조
 
 <br>
 
-## 페이지별 기능
+### 📅 개발 기간
+
+🌱 기획 및 디자인 : 2024.11.04 - 2024.11.08
+🌱 UI 구현 : 2024.11.09 - 2024.11.15
+🌱 기능 구현 : 2024.11.15 - 2024.11.29
+
+<br>
+
+## 🔗 페이지별 기능
--- src/common/GlobalStyle.tsx
+++ src/common/GlobalStyle.tsx
@@ -1,35 +1,41 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
+import PretendardRegularWoff2 from '../assets/fonts/pretendard/Pretendard-Regular.woff2';
+import PretendardRegularWoff from '../assets/fonts/pretendard/Pretendard-Regular.woff';
+import PretendardMediumWoff2 from '../assets/fonts/pretendard/Pretendard-Medium.woff2';
+import PretendardMediumWoff from '../assets/fonts/pretendard/Pretendard-Medium.woff';
+import PretendardBoldWoff2 from '../assets/fonts/pretendard/Pretendard-Bold.woff2';
+import PretendardBoldWoff from '../assets/fonts/pretendard/Pretendard-Bold.woff';
 
 export const GlobalStyle = createGlobalStyle`
   ${reset}
 
   @font-face {
     font-family: 'Pretendard-Regular';
-    src: url("../assets/fonts/pretendard/Pretendard-Regular.woff2") format("woff2"),
-         url("../assets/fonts/pretendard/Pretendard-Regular.woff") format("woff");
+    src: url(${PretendardRegularWoff2}) format("woff2"),
+         url(${PretendardRegularWoff}) format("woff");
     font-weight: 400;
     font-style: normal;
   }
   @font-face {
     font-family: 'Pretendard-Medium';
-    src: url("../assets/fonts/pretendard/Pretendard-Medium.woff2") format("woff2"),
-         url("../assets/fonts/pretendard/Pretendard-Medium.woff") format("woff");
+    src: url(${PretendardMediumWoff2}) format("woff2"),
+         url(${PretendardMediumWoff}) format("woff");
     font-weight: 500;
     font-style: normal;
   }
   @font-face {
     font-family: 'Pretendard-Bold';
-    src: url("../assets/fonts/pretendard/Pretendard-Bold.woff2") format("woff2"),
-         url("../assets/fonts/pretendard/Pretendard-Bold.woff") format("woff");
+    src: url(${PretendardBoldWoff2}) format("woff2"),
+         url(${PretendardBoldWoff}) format("woff");
     font-weight: 700;
     font-style: normal;
   }
 
   body {
-    margin: 0;
     font-family: 'Pretendard-Regular', sans-serif;
-    background-color: #ffffff;
     color: #333333;
+    background-color: #ffffff;
+    margin: 0;
   }
 `;
