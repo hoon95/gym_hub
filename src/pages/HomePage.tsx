f6d76e3a07741c2965db58a0138ae2e0e323ff35
hoon95 <maruon95@gmail.com>
2024-12-05 21:11:16 +0900

    ♻️ ESLint Refactoring

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,6 @@
 import { SectionsContainer } from 'react-fullpage';
-import styled from '@emotion/styled';
 import { Home } from "../components/Home/Main/Main";
 import { Step } from "../components/Home/Step/Step";
-// import { Footer } from "../components/Footer/Footer";
 import { Share } from 'components/Home/Share/Share';
 
 export const HomePage = () => {
